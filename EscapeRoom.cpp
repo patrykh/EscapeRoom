d0ce9c5c8e9b2a3697ddc9ed4812f1649b2d1bd9
patrykh <holda.patryk@gmail.com>
2015-08-25 21:10:36 +0200

    Begin
    Początek

---
+++ EscapeRoom.cpp
@@ -0,0 +1,25 @@
+// EscapeRoom.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio"
+
+int main()
+{
+	//Dane
+	// numer pokoju gdzi jest J23
+	// gdzie jest Bruner
+	// gdzie jest wyjœcie
+
+	// n - ca³kowita liczba pokoi
+
+	int j24, 
+		bruner, 
+		wyjscie,
+		liczbaPokoi;
+
+	cin >> j24 >> bruner >> wyjscie;
+	cin >>
+	return 0;
+}
+
